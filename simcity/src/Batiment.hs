7f34d3ccba695e478a0eca203ec530111de1e7ad
TSM540 <93137330+TSM540@users.noreply.github.com>
2024-04-04 23:03:23 +0200

    finished la partie batiment

--- simcity/src/Batiment.hs
+++ simcity/src/Batiment.hs
@@ -1,5 +1,5 @@
 module Batiment where
-import Forme 
+import Forme
 import Types
 
 data Batiment = Cabane {
@@ -38,3 +38,55 @@ instance Show Batiment where
     show (Atelier forme zoneId _ capacite employes) = "Atelier " ++ show forme ++ " dans la zone " ++ show zoneId ++ " avec une capacité de " ++ show capacite ++ " et " ++ show (length employes) ++ " employés"
     show (Epicerie forme zoneId _ capacite clients) = "Epicerie " ++ show forme ++ " dans la zone " ++ show zoneId ++ " avec une capacité de " ++ show capacite ++ " et " ++ show (length clients) ++ " clients"
     show (Commissariat forme zoneId _) = "Commissariat " ++ show forme ++ " dans la zone " ++ show zoneId
+
+-- fonctions pour gérer notre batiment
+-- on a supposé que les seul modifications possibles sont les cas dessus car c'est plutot pas trop réel de changer le type d'un batiment ou son entrée
+-- voir qu'il y a des cas dans la vraie vie où on peut changer le type d'un batiment mais ce n'est pas un généralité
+ajouterCapacite :: Batiment -> Int -> Batiment
+ajouterCapacite (Cabane forme zoneId entree capacite habitants) n = Cabane forme zoneId entree (capacite + n) habitants
+ajouterCapacite (Atelier forme zoneId entree capacite employes) n = Atelier forme zoneId entree (capacite + n) employes
+ajouterCapacite (Epicerie forme zoneId entree capacite clients) n = Epicerie forme zoneId entree (capacite + n) clients
+ajouterCapacite (Commissariat forme zoneId entree) _ = Commissariat forme zoneId entree
+
+diminuerCapacite :: Batiment -> Int -> Batiment
+diminuerCapacite (Cabane forme zoneId entree capacite habitants) n = Cabane forme zoneId entree (max (capacite - n) 0) habitants
+  --                                                                                        le max c'est juste pour ne pas avoir des valeurs négatives
+diminuerCapacite (Atelier forme zoneId entree capacite employes) n = Atelier forme zoneId entree (max (capacite - n) 0) employes
+diminuerCapacite (Epicerie forme zoneId entree capacite clients) n = Epicerie forme zoneId entree (max (capacite - n) 0) clients
+diminuerCapacite (Commissariat forme zoneId entree) _ = Commissariat forme zoneId entree
+
+ajouterCitoyen :: Batiment -> CitId -> Batiment
+ajouterCitoyen (Cabane forme zoneId entree capacite habitants) citId = Cabane forme zoneId entree capacite (citId:habitants)
+ajouterCitoyen (Atelier forme zoneId entree capacite employes) citId = Atelier forme zoneId entree capacite (citId:employes)
+ajouterCitoyen (Epicerie forme zoneId entree capacite clients) citId = Epicerie forme zoneId entree capacite (citId:clients)
+ajouterCitoyen (Commissariat forme zoneId entree) _ = Commissariat forme zoneId entree
+
+retirerCitoyen :: Batiment -> CitId -> Batiment
+retirerCitoyen (Cabane forme zoneId entree capacite habitants) citId = Cabane forme zoneId entree capacite (filter (/= citId) habitants)
+retirerCitoyen (Atelier forme zoneId entree capacite employes) citId = Atelier forme zoneId entree capacite (filter (/= citId) employes)
+retirerCitoyen (Epicerie forme zoneId entree capacite clients) citId = Epicerie forme zoneId entree capacite (filter (/= citId) clients)
+retirerCitoyen (Commissariat forme zoneId entree) _ = Commissariat forme zoneId entree
+
+
+-- des exemples
+cabane = Cabane (Rectangle (C 0 0) 10 10) (ZonId 1) (C 5 8) 10 []
+-- capacité est de 10
+new = ajouterCapacite cabane 5
+-- >>> show new
+-- "Cabane Rectangle (C {cx = 0, cy = 0}) 10 10 dans la zone ZonId 1 avec une capacit\233 de 15 et 0 habitants"
+new' = diminuerCapacite new 5
+-- >>> show new'
+-- "Cabane Rectangle (C {cx = 0, cy = 0}) 10 10 dans la zone ZonId 1 avec une capacit\233 de 10 et 0 habitants"
+new'_ = diminuerCapacite new 20
+-- >>> show new'_
+-- "Cabane Rectangle (C {cx = 0, cy = 0}) 10 10 dans la zone ZonId 1 avec une capacit\233 de 0 et 0 habitants"
+citoyen1 = CitId 1
+
+new'' = ajouterCitoyen cabane citoyen1
+
+-- >>> show new''
+-- "Cabane Rectangle (C {cx = 0, cy = 0}) 10 10 dans la zone ZonId 1 avec une capacit\233 de 10 et 1 habitants"
+new''' = retirerCitoyen new'' citoyen1
+-- >>> show new'''
+-- "Cabane Rectangle (C {cx = 0, cy = 0}) 10 10 dans la zone ZonId 1 avec une capacit\233 de 10 et 0 habitants"
+
